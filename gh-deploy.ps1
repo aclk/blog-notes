81aa552d49c9eeffdf2dbc39e1b2baf37f712fbb
he wenqing <hwq@wuxixinxiang.com>
2021-07-16 15:43:35 +0800

    modify path with repo

--- gh-deploy.ps1
+++ gh-deploy.ps1
@@ -2,6 +2,6 @@
 cd ..\aclk.github.io
 git pull
 # deploying with --dirty to skip an error with using the git-revision-date plugin
-mkdocs gh-deploy --dirty --config-file ../blog-notes/mkdocs.yml --remote-branch vk
+mkdocs gh-deploy --dirty --config-file ../notes/mkdocs.yml --remote-branch vk
 git reset --hard
-cd ..\blog-notes
+cd ..\notes
